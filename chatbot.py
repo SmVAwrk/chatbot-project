7139adf1cd8b5ae75228da5c5e19581c3dda7ebd
DS <doping5@list.ru>
2020-11-13 22:36:03 +0300

    Рефактор кода и добавление логгинга

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,42 @@
-from pprint import pprint
+
+from settings import TOKEN, GROUP_ID
+
 from random import randint
 
 import vk_api
 
 from vk_api import bot_longpoll
 
-from _token import token
+import logging
 
-group_id = 200224352
+log = logging.getLogger('debug_logger')
+log.setLevel(logging.DEBUG)
+
+
+def log_function():
+
+    file_handler = logging.FileHandler(filename='log/bot_debug.log', mode='a', encoding='utf-8')
+    format_for_dbg = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s',
+                                       datefmt='%d-%m-%Y %H:%M')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(format_for_dbg)
+    log.addHandler(file_handler)
+
+    strm_handler = logging.StreamHandler()
+    format_for_info = logging.Formatter(fmt='%(levelname)s - %(message)s')
+    strm_handler.setLevel(logging.DEBUG)
+    strm_handler.setFormatter(format_for_info)
+    log.addHandler(strm_handler)
 
 
 class ChatBot:
 
     def __init__(self, group_id, token):
+        """
+        Создание чат-бота
+        :param group_id: ID группы вк
+        :param token: индивидуальный токен из группы
+        """
         self.group_id = group_id
         self.token = token
         self.vk_api = vk_api.VkApi(token=self.token)
@@ -20,28 +44,37 @@ class ChatBot:
         self.api = self.vk_api.get_api()
 
     def run(self):
+        """
+        Запуск чат-бота
+        """
         for event in self.bot_longpoll.listen():
-            print(f"_______Произошло событие_______")
+            log.debug("_______Произошло событие_______")
             try:
                 self.in_action(event=event)
             except Exception as exc:
-                print(exc)
+                log.exception('Произошла ошибка')
 
     def in_action(self, event):
+        """
+        Обработка события
+        :param event: событие класса VkBotEvent(VkBotEventType)
+        """
         if event.type is vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
             u_id = event.object.message['from_id']
             u_text = event.object.message['text']
-            print(f"Пришло сообщение: {u_text}")
+            log.info(f"Пришло сообщение: {u_text}")
             self.api.messages.send(random_id=randint(0, 2 ** 20), user_id=u_id, message=f'Ваше сообщение: {u_text}')
         elif event.type is vk_api.bot_longpoll.VkBotEventType.MESSAGE_REPLY:
-            print('Я ответил на сообщение')
+            log.info('Я ответил на сообщение')
         else:
-            print(f'Я не умею обрабатывать событие: {event.type}')
+            log.debug(f'Я не умею обрабатывать событие: {event.type}')
 
 
 if __name__ == "__main__":
-    chat_bot = ChatBot(group_id=group_id, token=token)
+    log_function()
+    chat_bot = ChatBot(group_id=GROUP_ID, token=TOKEN)
     chat_bot.run()
+
 
 
 
